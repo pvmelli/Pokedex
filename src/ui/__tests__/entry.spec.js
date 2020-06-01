86cf4c97b6f87a83936b9bedf8ce9f8c55ca27df
pvmelli <paula.v.melli@gmail.com>
2020-06-01 03:24:02 -0300

    Updated jest tests

--- src/ui/__tests__/entry.spec.js
+++ src/ui/__tests__/entry.spec.js
@@ -4,6 +4,8 @@ import blastoiseSingle from "../../../cypress/fixtures/blastoise_single.json"
 import blastoiseSpecies from "../../../cypress/fixtures/blastoise_species.json"
 import flygonSingle from "../../../cypress/fixtures/flygon_single.json"
 import flygonSpecies from "../../../cypress/fixtures/flygon_species.json"
+import zeraoraSingle from "../../../cypress/fixtures/zeraora_single.json"
+import zeraoraSpecies from "../../../cypress/fixtures/zeraora_species.json"
 
 test("Manage creation of a pokedex entry", () => {
     document.body.innerHTML = fixture;
@@ -11,6 +13,7 @@ test("Manage creation of a pokedex entry", () => {
 
     managePokedexEntry(blastoiseSingle, blastoiseSpecies);
     managePokedexEntry(flygonSingle, flygonSpecies);
+    managePokedexEntry(zeraoraSingle, zeraoraSpecies);
 
     expect(Element.prototype.scrollIntoView).toHaveBeenCalled;
 
